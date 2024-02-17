04af7e5979d62a4a7dbd7dbe23131f7e5624a57b
Ketif2 <kevin.revelo01@epn.edu.ec>
2024-02-16 22:36:05 -0500

    Tarea 03

--- Tarea03/app/src/main/java/com/tarea03/rv_signal/StatusView.kt
+++ Tarea03/app/src/main/java/com/tarea03/rv_signal/StatusView.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class StatusView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.item_status_view)
+        setContentView(R.layout.activity_status)
     }
 }
